94cb3068ee56ae2d8c232b6267637e0ba39bcd30
amaybaum <amaybaum@users.noreply.github.com>
2024-10-15 11:45:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 import sys
 import logging
-import pyyaml
-import jinja2
-
+from yaml import Loader, Dumper
+from jinja2 import Template
 from aiohttp.web import run_app
-
 from sqli.app import init as init_app
+
+data = load(stream, Loader=Loader)
+template = Template('Hello {{ name }}!')
+template.render(name='John Doe')
 
 log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-
     app = init_app(sys.argv[1:])
-
     host = app['config']['app']['host']
     port = app['config']['app']['port']
     log.info(f'App is listening at http://{host}:{port}')
